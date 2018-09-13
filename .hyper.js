56ebc5944351a94a7b37d9965604f7b3b00b7991
Brad Garropy <bradgarropy@gmail.com>
2018-09-13 11:51:33 -0500

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -27,7 +27,7 @@ module.exports = {
         cursorAccentColor: '#000',
 
         // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-        cursorShape: 'BLOCK',
+        cursorShape: 'BEAM',
 
         // set to `true` (without backticks and without quotes) for blinking cursor
         cursorBlink: false,
